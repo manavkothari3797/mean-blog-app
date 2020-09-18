cd25faeaef34e48c94de19cb9999643e6a925bde
Manav Kothari <manav.kothari@openxcellinc.com>
2020-09-18 16:12:01 +0530

    servere file change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static('./dist/MeanBlog'));
 app.get('/*', (req, res) => {
-    res.sendFile(path.join((__dirname, '/dist/MeanBlog/index.html')));
+    res.sendFile(__dirname+ '/dist/MeanBlog/index.html');
 });
 
 app.listen(process.env.PORT || 3000, () => {
